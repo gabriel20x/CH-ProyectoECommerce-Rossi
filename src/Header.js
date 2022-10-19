c052e07696fa8e920c29a62f7c50adee429c627b
Franco R Rossi <francorossi712@gmail.com>
2022-10-19 00:30:38 -0300

    Primer Commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./Nav"
+
+function Header(){
+    return (
+        <header>
+            <h1>TituloTienda</h1>
+            <NavBar/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function NavBar (){
+    return (
+        <nav>
+            <ul class="nav_links">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Sobre nosotros</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
